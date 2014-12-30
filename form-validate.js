85bb7b1d46140de683fb936a35436571af11ddfe
Rushy Panchal <panchal.rushy@gmail.com>
2014-12-30 16:54:01 -0500

    Enable strict-mode JavaScript for contained globals

--- form-validate.js
+++ form-validate.js
@@ -2,58 +2,63 @@
 // form-validate.js
 // jQuery-based form validation
 
-var FORM_VALIDATORS = {
-	text: /.+/,
-	textarea: /.+/,
-	email: /.+\@.+\..+/
-	};
+(function ($) {
+	'use strict';
+	var jQuery = $;
 
-function validateForm(formID, options) {
-	// Validate a form by ID, same as $(formID).validate();
-	$(formID).validate(options);
-	}
+	var FORM_VALIDATORS = {
+		text: /.+/,
+		textarea: /.+/,
+		email: /.+\@.+\..+/
+		};
 
-jQuery.fn.validate = function(options) {
-	// Validate a form (onsubmit), through jQuery's selector
-	var options = jQuery.extend({onchange: true, errorClass: "error", successClass: "success", validators: {}}, options);
-	options.validators = jQuery.extend(FORM_VALIDATORS, options.validators);
-	var form = this.get(0);
-	var formElements = form.elements;
- 	var thisFormValidate = function() {
- 		var validForm = true;
- 		for (var index = 0; index < formElements.length; index++) {
-			var elem = formElements[index];
-			validForm = validateElement(elem, options) && validForm;
-			}
-		return validForm;
- 		}
-
- 	form.onsubmit = function() {
- 		if (thisFormValidate()) {
- 			this.submit();
- 			return true;
- 			}
- 		return false;
- 		};
-
- 	if (options.onchange) {
- 		$(formElements).on("change keyup paste input", function () { // check the elements when they change
-	 		validateElement(this, options);
-	 		});
- 		}
-	}
-
-function validateElement(elem, options) {
-	// Validate a specific element
-	if (elem.type in options.validators) {
-		if (options.validators[elem.type].test(elem.value)) { // Success!
-			$(elem).removeClass(options.errorClass).addClass(options.successClass);
-			return true;
-			}
-		else { // Invalid :(
-			$(elem).removeClass(options.successClass).addClass(options.errorClass);
-			return false;
-			}
+	function validateForm(formID, options) {
+		// Validate a form by ID, same as $(formID).validate();
+		$(formID).validate(options);
 		}
-	return true;
-	}
+
+	jQuery.fn.validate = function(options) {
+		// Validate a form (onsubmit), through jQuery's selector
+		var options = jQuery.extend({onchange: true, errorClass: "error", successClass: "success", validators: {}}, options);
+		options.validators = jQuery.extend(FORM_VALIDATORS, options.validators);
+		var form = this.get(0);
+		var formElements = form.elements;
+	 	var thisFormValidate = function() {
+	 		var validForm = true;
+	 		for (var index = 0; index < formElements.length; index++) {
+				var elem = formElements[index];
+				validForm = validateElement(elem, options) && validForm;
+				}
+			return validForm;
+	 		}
+
+	 	form.onsubmit = function() {
+	 		if (thisFormValidate()) {
+	 			this.submit();
+	 			return true;
+	 			}
+	 		return false;
+	 		};
+
+	 	if (options.onchange) {
+	 		$(formElements).on("change keyup paste input", function () { // check the elements when they change
+		 		validateElement(this, options);
+		 		});
+	 		}
+		}
+
+	function validateElement(elem, options) {
+		// Validate a specific element
+		if (elem.type in options.validators) {
+			if (options.validators[elem.type].test(elem.value)) { // Success!
+				$(elem).removeClass(options.errorClass).addClass(options.successClass);
+				return true;
+				}
+			else { // Invalid :(
+				$(elem).removeClass(options.successClass).addClass(options.errorClass);
+				return false;
+				}
+			}
+		return true;
+		}
+	}(jQuery));
